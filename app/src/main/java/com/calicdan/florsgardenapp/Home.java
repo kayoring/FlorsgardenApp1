b35aa431c2a3d660d64000ab7600d72312080fec
kayoring <113662501+kayoring@users.noreply.github.com>
2022-09-21 14:59:42 +0800

    buttons@home

--- app/src/main/java/com/calicdan/florsgardenapp/Home.java
+++ app/src/main/java/com/calicdan/florsgardenapp/Home.java
@@ -1,29 +1,44 @@
 package com.calicdan.florsgardenapp;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Home extends AppCompatActivity {
-    View storebtn;
+    View homebtn,forumbtn,storebtn,notificationbtn,chatbtn,profilebtn;
+    FloatingActionButton imageRecog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
+        buttons();
+    }
 
-        FloatingActionButton imageRecog = (FloatingActionButton) findViewById(R.id.imageRecog);
+    private void buttons() {
+        homebtn = findViewById(R.id.homebtn);
+        forumbtn = findViewById(R.id.forumbtn);
         storebtn = findViewById(R.id.storebtn);
-
-        imageRecog.setOnClickListener(new View.OnClickListener() {
+        imageRecog = (FloatingActionButton) findViewById(R.id.imageRecog);
+        notificationbtn = findViewById(R.id.notificationbtn);
+        chatbtn = findViewById(R.id.chatbtn);
+        profilebtn = findViewById(R.id.profilebtn);
+        
+        homebtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Home.this, ImageRecognition.class));
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, Home.class));
+
+            }
+        });
+
+        forumbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, StoreActivity.class));
+
             }
         });
 
@@ -35,5 +50,35 @@ public class Home extends AppCompatActivity {
             }
         });
 
+        imageRecog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Home.this, ImageRecognition.class));
+            }
+        });
+
+        notificationbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, StoreActivity.class));
+
+            }
+        });
+
+        chatbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, StoreActivity.class));
+
+            }
+        });
+
+        profilebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, StoreActivity.class));
+
+            }
+        });
     }
 }
