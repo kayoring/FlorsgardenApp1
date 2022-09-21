0888e5e5ffe38494d120501cda672bfebaa7c3f5
kayoring <113662501+kayoring@users.noreply.github.com>
2022-09-21 14:35:13 +0800

    home/storebtn

--- app/src/main/java/com/calicdan/florsgardenapp/Home.java
+++ app/src/main/java/com/calicdan/florsgardenapp/Home.java
@@ -1,16 +1,27 @@
 package com.calicdan.florsgardenapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
+    View storebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
+        storebtn = findViewById(R.id.storebtn);
+
+        storebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Home.this, StoreActivity.class));
+            }
+        });
 
     }
 }
