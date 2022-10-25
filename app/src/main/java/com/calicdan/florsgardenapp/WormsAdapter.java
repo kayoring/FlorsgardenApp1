2c94896d872b943001cb497cd7a2216428a994cd
grbllcnls <gabriellecanlas6@gmail.com>
2022-10-25 13:56:30 +0800

    Guides Edit Pop Up

--- app/src/main/java/com/calicdan/florsgardenapp/WormsAdapter.java
+++ app/src/main/java/com/calicdan/florsgardenapp/WormsAdapter.java
@@ -3,6 +3,9 @@ package com.calicdan.florsgardenapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
 
 import java.util.Objects;
 
@@ -41,6 +46,36 @@ public class WormsAdapter extends FirebaseRecyclerAdapter<HomeModel,WormsAdapter
                 .error(R.drawable.common_google_signin_btn_icon_dark_normal)
                 .into(holder.profileImage);
 
+
+
+        holder.imgEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.profileImage.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.update_popup))
+                        .setExpanded(true,1500)
+                        .create();
+
+
+
+                View view = dialogPlus.getHolderView();
+
+                EditText name = view.findViewById(R.id.txtName);
+                EditText description = view.findViewById(R.id.txtDesc);
+                EditText Surl = view.findViewById(R.id.txtSurl);
+
+                Button btnUpdate = view.findViewById(R.id.btnUpdate);
+
+
+                name.setText(model.getName());
+                description.setText(model.getDescription());
+                Surl.setText(model.getSurl());
+
+                dialogPlus.show();
+
+            }
+        });
+
     }
 
     @NonNull
@@ -56,6 +91,10 @@ public class WormsAdapter extends FirebaseRecyclerAdapter<HomeModel,WormsAdapter
         TextView nameText;
         TextView descriptionText;
 
+        ImageView imgEdit;
+
+        Button btnDelete;
+
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
 
@@ -64,6 +103,9 @@ public class WormsAdapter extends FirebaseRecyclerAdapter<HomeModel,WormsAdapter
             nameText = (TextView)itemView.findViewById(R.id.nameText);
 
             descriptionText = (TextView)itemView.findViewById(R.id.descriptionText);
+
+            imgEdit = (ImageView)itemView.findViewById(R.id.imgEdit);
+            btnDelete = (Button)itemView.findViewById(R.id.btnDelete);
 
 
         }
